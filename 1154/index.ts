8032e0df89b46a8c254f59cba9da3d542f6d3a0b
Yang Nan <nanyangcn@gmail.com>
2021-08-10 17:02:24 +0300

    1154

--- 1154/index.ts
+++ 1154/index.ts
@@ -1,29 +1,32 @@
 function dayOfYear(date: string): number {
   const [year, month, day] = date.split('-');
 
-  const numOfDays = {
+  const numOfDays: { [index: number]: number } = {
     '0': 0,
     '1': 31,
-    '2': 28 + 31,
-    '3': 31 + 28 + 31,
-    '4': 30 + 31 + 28 + 31,
-    '5': 31 + 30 + 31 + 28 + 31,
-    '6': 30 + 31 + 30 + 31 + 28 + 31,
-    '7': 31 + 30 + 31 + 30 + 31 + 28 + 31,
-    '8': 31 + 31 + 30 + 31 + 30 + 31 + 28 + 31,
-    '9': 30 + 31 + 31 + 30 + 31 + 30 + 31 + 28 + 31,
-    '10': 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31 + 28 + 31,
-    '11': 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31 + 28 + 31,
-    '12': 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31 + 28 + 31,
+    '2': 28,
+    '3': 31,
+    '4': 30,
+    '5': 31,
+    '6': 30,
+    '7': 31,
+    '8': 31,
+    '9': 30,
+    '10': 31,
+    '11': 30,
   };
+
   if (Number(year) % 100 === 0) {
-    if (Number(year) % 400 === 0) numOfDays['2'] = 29 + 31;
+    if (Number(year) % 400 === 0) numOfDays[2] = 29;
   } else {
-    if (Number(year) % 4 === 0) numOfDays['2'] = 29 + 31;
+    if (Number(year) % 4 === 0) numOfDays[2] = 29;
   }
 
-  const result = numOfDays[Number(month) - 1] + Number(day);
-  return result;
+  let result = 0;
+  for (let i = 0; i < Number(month); i++) {
+    result += numOfDays[i];
+  }
+  return result + Number(day);
 }
 
 console.log(dayOfYear('2004-03-01'));
